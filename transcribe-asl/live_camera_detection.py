0c78eed3579ab7cba0b9534f55b64700ca5efdad
Jimmy Scray <eugene.j.scray@gmail.com>
2024-03-06 10:39:00 -0600

    Removing face, and removing dependency on ip address...

--- transcribe-asl/live_camera_detection.py
+++ transcribe-asl/live_camera_detection.py
@@ -2,81 +2,64 @@ import cv2
 import mediapipe as mp
 import time
 
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_holistic = mp.solutions.holistic
 
+import cv2
 
-cap = cv2.VideoCapture(0)
-# You'll probably have to change this
-# If you're using a webcam, you can comment this out
-# If you're using a phone, you can use an app like IP Webcam
-# and use the URL provided by the app
-address = "http://192.168.15.35:8080/video"
-cap.open(address)
-# cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+def main():
+    
+    mp_drawing = mp.solutions.drawing_utils
+    mp_drawing_styles = mp.solutions.drawing_styles
+    mp_holistic = mp.solutions.holistic
+    
+    # access webcam
+    cap = cv2.VideoCapture(0)
+    with mp_holistic.Holistic(
+        model_complexity=0,
+        smooth_landmarks=True,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5,
+        enable_segmentation=True,
+        refine_face_landmarks=True,
+    ) as holistic:
+        while True:
+            # pull frame
+            ret, frame = cap.read()
+            # mirror frame
+            frame = cv2.flip(frame, 1)
+            # display frame
+            cv2.imshow('frame',frame)
+            if cv2.waitKey(1) == ord('q'):
+                break
+            
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            results = holistic.process(frame)
+            
+            # print(results.pose_landmarks)
 
-with mp_holistic.Holistic(
-    model_complexity=0,
-    smooth_landmarks=True,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5,
-    enable_segmentation=True,
-    refine_face_landmarks=True,
-) as holistic:
-    while cap.isOpened():
-        success, image = cap.read()
-        if not success:
-            print("Ignoring empty camera frame.")
-            # If loading a video, use 'break' instead of 'continue'.
-            continue
+            # Draw landmark annotation on the frame.
+            frame.flags.writeable = True
+            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+            mp_drawing.draw_landmarks(
+                frame,
+                results.pose_landmarks,
+                mp_holistic.POSE_CONNECTIONS,
+                landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style(),
+            )
+            mp_drawing.draw_landmarks(
+                frame,
+                results.left_hand_landmarks,
+                mp_holistic.HAND_CONNECTIONS,
+                landmark_drawing_spec=mp_drawing_styles
+                .get_default_pose_landmarks_style())
+            cv2.imshow("MediaPipe Holistic", frame)
+            fps = 1/(end-start)
+            # TODO: Insert pose_recognized code here... Import module and run command.
+            cv2.putText(image, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 2)
+            
+#         print(fps)
+    # release everything
+    cap.release()
+    cv2.destroyAllWindows()
 
-        start = time.time()
-
-        # To improve performance, optionally mark the image as not writeable to
-        # pass by reference.
-        image.flags.writeable = False
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        results = holistic.process(image)
-
-        # Draw landmark annotation on the image.
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        mp_drawing.draw_landmarks(
-            image,
-            results.face_landmarks,
-            mp_holistic.FACEMESH_CONTOURS,
-            landmark_drawing_spec=None,
-            connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_contours_style(),
-        )
-        mp_drawing.draw_landmarks(
-            image,
-            results.pose_landmarks,
-            mp_holistic.POSE_CONNECTIONS,
-            landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style(),
-        )
-        mp_drawing.draw_landmarks(
-            image,
-            results.left_hand_landmarks,
-            mp_holistic.HAND_CONNECTIONS,
-            landmark_drawing_spec=mp_drawing_styles
-            .get_default_pose_landmarks_style())
-
-        mp_drawing.draw_landmarks(
-            image,
-            results.right_hand_landmarks,
-            mp_holistic.HAND_CONNECTIONS,
-            landmark_drawing_spec=mp_drawing_styles
-            .get_default_pose_landmarks_style())
-
-        # end time
-        end = time.time()
-
-        fps = 1/(end-start)
-        print(fps)
-        cv2.putText(image, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 2)
-        # Flip the image horizontally for a selfie-view display.
-        cv2.imshow("MediaPipe Holistic", image)
-        if cv2.waitKey(5) & 0xFF == 27:
-            break
-cap.release()
+if __name__ == "__main__":
+   main()
