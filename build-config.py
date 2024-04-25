e505cbaad837eb2a3a232d0b6c4f9a77ba12f61e
Jimmy Scray <eugene.j.scray@gmail.com>
2024-04-25 16:32:01 -0500

    Adding build process

---
+++ build-config.py
@@ -0,0 +1,8 @@
+import os, json, glob 
+config_template = {"packages": ["numpy"], "files": {"/media_pipe/gesture/base.py": ""}}
+models = glob.glob("./transcribe-asl/gesture/base_poses_hf/*.csv")
+for model in models:
+  model = model.replace('./transcribe-asl', '/media_pipe')
+  config_template["files"][model] = ""
+print(json.dumps(config_template))
+
