00b1ec50bfc2538428b4fa3a4b76572a26b21932
sehnemroger <roger@sehnem.com>
2024-05-31 08:40:49 -0300

    Exercise poses

--- transcribe-asl/data_in/from_images.py
+++ transcribe-asl/data_in/from_images.py
@@ -18,7 +18,10 @@ mp_holistic = mp.solutions.holistic
 # Excute in parallel?
 execute_parallel = False
 
-images = glob.glob("images_of_gestures/*.jpg")
+images_png = glob.glob("references/body_images_references/*.png")
+images_jpg = glob.glob("references/body_images_references/*.jpg")
+images = images_png + images_jpg
+whole_body = True
 
 
 def transcribe_word(image_file):
@@ -27,103 +30,103 @@ def transcribe_word(image_file):
     left_name = f"{name}_Transcription_Left_Hand"
     rigth_name = f"{name}_Transcription_Right_Hand"
 
-    if (
-        not os.path.isfile(f"./gesture/base_poses_hf/{pose_name}.csv")
-        or not os.path.isfile(f"./gesture/base_poses_hf/{left_name}.csv")
-        or not os.path.isfile(f"./gesture/base_poses_hf/{rigth_name}.csv")
-    ):
-        image = cv2.imread(f"{image_file}")
+    # if (
+    #     not os.path.isfile(f"./gesture/base_poses_hf/{pose_name}.csv")
+    #     or not os.path.isfile(f"./gesture/base_poses_hf/{left_name}.csv")
+    #     or not os.path.isfile(f"./gesture/base_poses_hf/{rigth_name}.csv")
+    # ):
+    image = cv2.imread(f"{image_file}")
 
-        print("Capturing feed and registering pose points")
-        with mp_holistic.Holistic(
-            model_complexity=1,
-            smooth_landmarks=True,
-            min_detection_confidence=0.5,
-            min_tracking_confidence=0.5,
-            enable_segmentation=True,
-            refine_face_landmarks=True,
-        ) as holistic:
-            # To improve performance, optionally mark the image as not writeable to
-            # pass by reference.
-            pose_csv = "index,x,y,z\n"
-            left_hand_csv = "index,x,y,z\n"
-            right_hand_csv = "index,x,y,z\n"
-            image.flags.writeable = False
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            results = holistic.process(image)
+    print("Capturing feed and registering pose points")
+    with mp_holistic.Holistic(
+        model_complexity=1,
+        smooth_landmarks=False,
+        min_detection_confidence=0.01,
+        min_tracking_confidence=0.01,
+        enable_segmentation=False,
+        refine_face_landmarks=True,
+    ) as holistic:
+        # To improve performance, optionally mark the image as not writeable to
+        # pass by reference.
+        pose_csv = "index,x,y,z\n"
+        left_hand_csv = "index,x,y,z\n"
+        right_hand_csv = "index,x,y,z\n"
+        image.flags.writeable = False
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = holistic.process(image)
 
-            if results.pose_landmarks:
-                for index, data_point in enumerate(results.pose_landmarks.landmark):
+        if results.pose_landmarks:
+            for index, data_point in enumerate(results.pose_landmarks.landmark):
 
-                    pose_csv = (
-                        pose_csv
-                        + f"{index},"
-                        + f"{data_point.x},"
-                        + f"{data_point.y},"
-                        + f"{data_point.z}\n"
-                    )
-
-            if results.right_hand_landmarks:
-                data = list(results.right_hand_landmarks.landmark)
-                for index, value in enumerate(data):
-                    right_hand_csv = (
-                        right_hand_csv
-                        + f"{index},"
-                        + f"{value.x},"
-                        + f"{value.y},"
-                        + f"{value.z} \n"
-                    )
-
-            if results.left_hand_landmarks:
-                data = list(results.left_hand_landmarks.landmark)
-                for index, value in enumerate(data):
-                    left_hand_csv = (
-                        left_hand_csv
-                        + f"{index},"
-                        + f"{value.x},"
-                        + f"{value.y},"
-                        + f"{value.z} \n"
-                    )
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-            while True:
-                mp_drawing.draw_landmarks(
-                    image,
-                    results.pose_landmarks,
-                    mp_holistic.POSE_CONNECTIONS,
-                    landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style(),
-                )
-                mp_drawing.draw_landmarks(
-                    image,
-                    results.left_hand_landmarks,
-                    mp_holistic.HAND_CONNECTIONS,
-                    landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style(),
+                pose_csv = (
+                    pose_csv
+                    + f"{index},"
+                    + f"{data_point.x},"
+                    + f"{data_point.y},"
+                    + f"{data_point.z}\n"
                 )
 
-                mp_drawing.draw_landmarks(
-                    image,
-                    results.right_hand_landmarks,
-                    mp_holistic.HAND_CONNECTIONS,
-                    landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style(),
+        if results.right_hand_landmarks:
+            data = list(results.right_hand_landmarks.landmark)
+            for index, value in enumerate(data):
+                right_hand_csv = (
+                    right_hand_csv
+                    + f"{index},"
+                    + f"{value.x},"
+                    + f"{value.y},"
+                    + f"{value.z} \n"
                 )
 
-                mp_drawing.draw_axis(image, np.eye(3), np.zeros(3))
-                image.flags.writeable = True
-                cv2.imshow("MediaPipe Holistic", image)
-                if cv2.waitKey(1) == ord("q"):
-                    break
+        if results.left_hand_landmarks:
+            data = list(results.left_hand_landmarks.landmark)
+            for index, value in enumerate(data):
+                left_hand_csv = (
+                    left_hand_csv
+                    + f"{index},"
+                    + f"{value.x},"
+                    + f"{value.y},"
+                    + f"{value.z} \n"
+                )
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-        file = open(f"./gesture/base_poses_hf/{pose_name}.csv", "w")
-        file.write(pose_csv)
-        file.close()
+        while True:
+            mp_drawing.draw_landmarks(
+                image,
+                results.pose_landmarks,
+                mp_holistic.POSE_CONNECTIONS,
+                landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style(),
+            )
+            mp_drawing.draw_landmarks(
+                image,
+                results.left_hand_landmarks,
+                mp_holistic.HAND_CONNECTIONS,
+                landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style(),
+            )
 
-        file = open(f"./gesture/base_poses_hf/{left_name}.csv", "w")
-        file.write(left_hand_csv)
-        file.close()
+            mp_drawing.draw_landmarks(
+                image,
+                results.right_hand_landmarks,
+                mp_holistic.HAND_CONNECTIONS,
+                landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style(),
+            )
 
-        file = open(f"./gesture/base_poses_hf/{rigth_name}.csv", "w")
-        file.write(right_hand_csv)
-        file.close()
+            mp_drawing.draw_axis(image, np.eye(3), np.zeros(3))
+            image.flags.writeable = True
+            cv2.imshow("MediaPipe Holistic", image)
+            if cv2.waitKey(1) == ord("q"):
+                break
+
+    file = open(f"./gesture/base_poses_hf/{pose_name}.csv", "w")
+    file.write(pose_csv)
+    file.close()
+
+    file = open(f"./gesture/base_poses_hf/{left_name}.csv", "w")
+    file.write(left_hand_csv)
+    file.close()
+
+    file = open(f"./gesture/base_poses_hf/{rigth_name}.csv", "w")
+    file.write(right_hand_csv)
+    file.close()
 
 
 if __name__ == "__main__":
